f42fd27392bbbe3fe562728a95f8babd7c099b11
= <=>
2023-05-03 03:33:42 +0600

    Second Commit. Updated code on MainActivity.java file

--- app/src/main/java/com/example/mycustomadapter/CustomAdapter.java
+++ app/src/main/java/com/example/mycustomadapter/CustomAdapter.java
@@ -1,15 +1,19 @@
 package com.example.mycustomadapter;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class CustomAdapter extends BaseAdapter {
 
     int[] flags;
     String[] countryNames;
     Context context;
+    private LayoutInflater inflater;
 
     CustomAdapter(Context context,String[] countryNames, int[] flags) {
         this.context = context;
@@ -20,7 +24,7 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public int getCount() {
-        return 0;
+        return countryNames.length;
     }
 
     @Override
@@ -35,6 +39,18 @@ public class CustomAdapter extends BaseAdapter {
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        return null;
+
+        if (convertView==null) {
+            inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = inflater.inflate(R.layout.sample_view,parent,false);
+        }
+
+        ImageView imageView = convertView.findViewById(R.id.imageViewId);
+        TextView textView = convertView.findViewById(R.id.countryNameId);
+
+        imageView.setImageResource(flags[position]);
+        textView.setText(countryNames[position]);
+
+        return convertView;
     }
 }
--- app/src/main/java/com/example/mycustomadapter/MainActivity.java
+++ app/src/main/java/com/example/mycustomadapter/MainActivity.java
@@ -3,7 +3,10 @@ package com.example.mycustomadapter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +23,14 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listViewId);
         CustomAdapter adapter = new CustomAdapter(this,countryNames,flags);
         listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = countryNames[position];
+                Toast.makeText(MainActivity.this, value, Toast.LENGTH_SHORT).show();
+            }
+        });
 
 
 
